8ea149f3078a4e8eda0f0e9ec36bf54cba5f4542
Santiago Gil Arboleda <sagilar@unal.edu.co>
2019-01-04 16:24:01 -0500

    Uploading first LoRaWAN 1.0.2 application

---
+++ Lorawan-v1-0-2-EIoT-1/eiot.h
@@ -0,0 +1,9 @@
+float readTemperatureRefrigerator();
+float readTemperatureFreezer();
+bool readDoorState();
+void readCurrentSensor();
+void calculateCurrent();
+float readMeanCurrent();
+float readMeanPower();
+float readMaxPower();
+double filter2o(double u, double &v1, double &v2, const double a0, const double a1, const double a2, const double b0, const double b1, const double b2);
